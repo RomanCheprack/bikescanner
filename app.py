dacd7b4464997954eb46bd520515cb0f7bb6a76d
roman cheprack <romancheprack@gmail.com>
2019-04-20 21:16:51 +0300

    added bike24 scraper

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, url_for
-from models import bike_discount, merlin_bikes
+from models import bike_discount, merlin_bikes, bike24
 import csv
 
 app = Flask(__name__)
@@ -14,11 +14,12 @@ def home():
 def search():
 	if request.method == 'POST':
 		search_term = request.form['search_term']
-		discount_res = bike_discount(search_term)
-		merlin_res = merlin_bikes(search_term)
-		with open('bike_discount.csv', 'r') as file:
+		bike_discount(search_term)
+		merlin_bikes(search_term)
+		bike24(search_term)
+		with open('database.csv', 'r') as file:
 			data = csv.DictReader(file)
-			return render_template('search.html',search_term=search_term, data=data, merlin_res=merlin_res)
+			return render_template('search.html',search_term=search_term, data=data)
 	else:
 		return render_template('search.html')
 
